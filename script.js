d9719d09d807aca5450e859a79f2a65cb3eb6c85
WhalesLoveSmash <dannyrhalsteadbackup@gmail.com>
2025-06-20 13:33:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+const dayStart = "07:30";
+const dayEnd = "17:45";
+
+function timeToMinutes(timeStr) {
+    let [hourStr, minuteStr] = timeStr.split(":");
+    let hours = parseInt(hourStr, 10);
+    let minutes = parseInt(minuteStr, 10);
+    let totalMinutes= (hours * 60) + minutes;
+    return totalMinutes
+}
+
+function scheduleMeeting(startTime,durationMinutes) {
+    let dayStartSinceMid = timeToMinutes(dayStart);
+    let dayEndSinceMid = timeToMinutes(dayEnd);
+
+    let startTimeMinSinceMid = timeToMinutes(startTime);
+    let finishTimeSinceMid = startTimeMinSinceMid + durationMinutes;
+    
+    let startsOnTime = startTimeMinSinceMid >= dayStartSinceMid;
+    let endsOnTime = finishTimeSinceMid <= dayEndSinceMid;
+    
+    return startsOnTime && endsOnTime;
+}
+
+//scheduleMeeting("7:00",15);     // false
+//scheduleMeeting("7:30",30);     // true
+
 /*
 Practicing Comparisons
 Let's practice working with value types and comparisons (Chapter 4, Pillar 3) where coercion will need to be involved.
@@ -6,21 +33,3 @@ scheduleMeeting(..) should take a start time (in 24-hour format as a string "hh:
 It should return true if the meeting falls entirely within the work day (according to the times specified in dayStart and dayEnd);
 return false if the meeting violates the work day bounds.
 */
-
-const dayStart = "07:30";
-const dayEnd = "17:45";
-
-function scheduleMeeting(startTime,durationMinutes) {
-    if (startTime && startTime + durationMinutes <= dayEnd){
-        //above just determined that meeting ends on time
-        //have not yet checked that it starts on time
-    }
-}
-
-scheduleMeeting("7:00",15);     // false
-scheduleMeeting("07:15",30);    // false
-scheduleMeeting("7:30",30);     // true
-scheduleMeeting("11:30",60);    // true
-scheduleMeeting("17:00",45);    // true
-scheduleMeeting("17:30",30);    // false
-scheduleMeeting("18:00",15);    // false
