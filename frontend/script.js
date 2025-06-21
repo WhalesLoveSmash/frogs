127474c6a40f9e637e96e9e3929a97a44e8e84d8
WhalesLoveSmash <dannyrhalsteadbackup@gmail.com>
2025-06-21 18:27:53 -0500

    refactoring

---
+++ frontend/script.js
@@ -0,0 +1,57 @@
+/*
+The range(..) function takes a number as its first argument, representing the first number in a desired range of numbers. 
+The second argument is also a number representing the end of the desired range (inclusive). If the second argument is omitted,
+then another function should be returned that expects that argument.
+Unlike Python there is no built in range method
+So This problem is essentally reverse engerning range but overcpmlicating it by
+using two functions and closures to let them comunicate
+*/
+
+
+//first we will do the non closure only one function super easy version
+function range(start, end) {
+    const dateRange = [];
+    for (let i = start; i <= end; i++) {
+        dateRange.push(i);
+    }
+    return dateRange;
+}
+
+console.log(range(5, 9));
+//then we have to do the actual problem of doing this in two problems with closures
+
+
+/*
+Practicing Closure
+Now let's practice with closure (Chapter 4, Pillar 1).
+
+range(3,3);    // [3]
+range(3,8);    // [3,4,5,6,7,8]
+range(3,0);    // []
+
+var start3 = range(3);
+var start4 = range(4);
+
+start3(3);     // [3]
+start3(8);     // [3,4,5,6,7,8]
+start3(0);     // []
+*/
+
+//Dom and Event Listener type stuff
+//This is the part chat GPT gave me to make my frog html change so I got to implement this part on my own above
+
+// Select the h1 by ID
+const frogTitle = document.getElementById("frogTitle");
+
+// Track whether it's currently showing the range
+let showingRange = false;
+
+// Attach a click event listener
+frogTitle.addEventListener("click", function () {
+    if (showingRange) {
+        frogTitle.textContent = "Frogs";
+    } else {
+        frogTitle.textContent = range(5, 9).join(",");
+    }
+    showingRange = !showingRange;
+});
