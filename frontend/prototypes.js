4f9fe6d4b1993cf613c2ccdf7a00496d6f791a45
WhalesLoveSmash <dannyrhalsteadbackup@gmail.com>
2025-06-22 17:33:52 -0500

    Add files via upload

--- frontend/prototypes.js
+++ frontend/prototypes.js
@@ -26,26 +26,28 @@ var reel = {
     }
 };
 
-/*
 var slotMachine = {
     reels: [
         Object.create(reel),
         Object.create(reel),
         Object.create(reel)
     ],
+    
     spin() {
         this.reels.forEach(function spinReel(reel) {
             reel.spin();
         });
     },
-*/
-
-var slotMachine = {
-    //reels
-}
-
+    
     display() {
-        // TODO
+        for (let rowOffset of [-1, 0, 1]) {
+            const row = this.reels.map(reel => {
+                const symbols = reel.symbols;
+                const pos = (reel.position + rowOffset + symbols.length) % symbols.length;
+                return symbols[pos];
+            }).join(" | ");
+            console.log(row);
+        }
     }
 };
 
